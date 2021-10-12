e6d19131faa0a193159f3a0daefc89f485a66b58
acwander <alexcwander@gmail.com>
2021-10-12 11:57:33 -0500

    details page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -25,9 +25,15 @@ function Detail() {
 					<img src="/images/play-icon-white.png" alt="Trailer" />
 					<span>TRAILER</span>
 				</TrailerButton>
-				<AddButton></AddButton>
-				<GroupWatchButton></GroupWatchButton>
+				<AddButton>
+					<span>+</span>
+				</AddButton>
+				<GroupWatchButton>
+					<img src="/images/group-icon.png" alt="Group Watch" />
+				</GroupWatchButton>
 			</Controls>
+			<SubTitle>2018 * 7m * Family, Fantasy, Kids, Animation</SubTitle>
+			<Description>Placeholder description</Description>
 		</Container>
 	);
 }
@@ -71,6 +77,7 @@ const ImageTitle = styled.div`
 
 const Controls = styled.div`
 	display: flex;
+	align-items: center;
 `;
 
 const PlayButton = styled.button`
@@ -97,6 +104,38 @@ const TrailerButton = styled(PlayButton)`
 	color: rgb(249, 249, 249);
 `;
 
-const AddButton = styled.button``;
+const AddButton = styled.button`
+	width: 44px;
+	height: 44px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	border-radius: 50%;
+	border: 2px solid white;
+	background-color: rgba(0, 0, 0, 0.6);
+	cursor: pointer;
+	margin-right: 16px;
 
-const GroupWatchButton = styled.button``;
+	span {
+		font-size: 30px;
+		color: white;
+	}
+`;
+
+const GroupWatchButton = styled(AddButton)`
+	background-color: rgba(0, 0, 0);
+`;
+
+const SubTitle = styled.div`
+	color: rgb(249, 249, 249);
+	font-size: 15px;
+	min-height: 20px;
+	margin-top: 26px;
+`;
+
+const Description = styled.div`
+	line-height: 1.4;
+	font-size: 20px;
+	margin-top: 16px;
+	color: rgb(249, 249, 249);
+`;
